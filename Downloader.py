5346bd2e3236de09fa485849f0c8dddfe3ab756f
kratosc2 <30858446+kratosc2@users.noreply.github.com>
2018-02-08 04:29:52 +0200

    Add files via upload

--- Downloader.py
+++ Downloader.py
@@ -6,30 +6,35 @@ from ffprobe import FFProbe
 from Tkinter import Tk
 import tkFileDialog 
 
-locat = '' 
+locat = '/root/Downloads/' 
 
 def tkfl():
     global locat
     locat = tkFileDialog.askdirectory() + '/'
-    print locat
+    
 
 
-app = gui('YouTubeAudioDownloader','1366x600')
+app = gui('YouTubeAudioDownloader','1366x650')
 
 class MyLogger(object):
     def debug(self,msg):
-        app.addListItem('l5',msg)
+        if msg[5:13] == 'download':
+            app.addListItem('l5',msg[14:])
+        else:
+            app.addListItem('l5',msg)
     def warning(self,msg):
         app.addListItem('l5',msg)
     def error(self,msg):
         app.addListItem('l5',msg)
+        print msg
 
 
 
 def my_hook(d):
-    print d['status']
+    app.setStatusbar(d['status'])
     if not d['status'] == 'finished':
         down = d['downloaded_bytes']
+        siz = d['total_bytes']
         filesiz = d['total_bytes']
         etc = down / (filesiz / 100)
         if etc > 0:
@@ -64,8 +69,9 @@ def press(btn):
                 mt3v = (str(mt1['view_count']) )
                 mt4d = ((str(mt1['duration'] / 60) + ':'+ str(mt1['duration'] % 60) ) )
                 mt5ud = (str(mt1['upload_date']) )
-                mt6f = (str(mt1['format']))
-                app.addGridRow('lb1',[mt1t,mt2u,mt3v,mt4d,mt5ud,mt6f])
+                mt6ex = (str(mt1['ext']))
+                mtf = (str(mt1['format']))
+                app.addGridRow('lb1',[mt1t,mt2u,mt3v,mt4d,mt5ud,mt6ex,mtf])
                 ydl.download([durl])
                 print locat
 
@@ -76,23 +82,31 @@ app.addEntry('e1',0,0)
 app.setEntryDefault('e1','URL')
 app.addButton('Download',press,0,1)
 app.setButtonSticky('Download','news')
-app.setButtonFg('Download','green')
+app.setButtonFg('Download','seagreen')
 app.addLabel('l2','Downloaded Items',1,0)
-app.setLabelFg('l2','green')
+app.setLabelSticky('l2','ews')
+app.addLabel('l7','',1,1)
+app.setLabelBg('l7','seagreen')
+app.setLabelSticky('l7','ews')
+app.setLabelBg('l2','seagreen')
 app.setEntrySticky('e1','news')
-app.addGrid('lb1',[['Title','Uploader','Views','Duration','Upload Date','Format']],2,0,2,2)
+app.addGrid('lb1',[['Title','Uploader','Views','Duration','Upload Date','Ext','Format']],2,0,2,2)
 app.setGridSticky('lb1','news')
 app.addLabelOptionBox('Format',['-Video-','best','-Audio-','bestaudio'],3,1)
 app.addLabel('l4','Logger',4,0)
+app.setLabelSticky('l4','ews')
 app.addLabel('l5b','',4,1)
-app.setLabelBg('l5b','green')
-app.setLabelBg('l4','green')
+app.setLabelSticky('l5b','ews')
+app.setLabelBg('l5b','seagreen')
+app.setLabelBg('l4','seagreen')
 app.addListBox('l5','',5,0,2,2)
 app.setListBoxSticky('l5','news')
 app.addMeter('progress',3,0)
-app.setMeterFill('progress','green')
+app.setMeterFill('progress','seagreen')
 app.addMenuList('File',['Select Download Folder'],tkfl)
-
+app.addStatusbar(fields=1,side="LEFT")
+app.setStatusbarBg('seagreen')
+app.setStatusbarWidth(200)
 
 app.go()
 
