db299d5ee5d09687fde5a35c8bdcd98d5936a231
kratosc2 <30858446+kratosc2@users.noreply.github.com>
2018-02-08 03:38:03 +0200

    Add files via upload

--- Downloader.py
+++ Downloader.py
@@ -3,32 +3,51 @@ import youtube_dl
 from appJar import gui
 import time
 from ffprobe import FFProbe
+from Tkinter import Tk
+import tkFileDialog 
 
-app = gui('YouTubeAudioDownloader','1000x550')
+locat = '' 
+
+def tkfl():
+    global locat
+    locat = tkFileDialog.askdirectory() + '/'
+    print locat
+
+
+app = gui('YouTubeAudioDownloader','1366x600')
 
 class MyLogger(object):
     def debug(self,msg):
-        app.queueFunction(app.addListItem,'l5',msg)
+        app.addListItem('l5',msg)
     def warning(self,msg):
-        app.queueFunction(app.addListItem,'l5',msg)
+        app.addListItem('l5',msg)
     def error(self,msg):
-        app.queueFunction(app.addListItem,'l5',msg)
+        app.addListItem('l5',msg)
 
+
+
+def my_hook(d):
+    print d['status']
+    if not d['status'] == 'finished':
+        down = d['downloaded_bytes']
+        filesiz = d['total_bytes']
+        etc = down / (filesiz / 100)
+        if etc > 0:
+            app.setMeter('progress',etc)
+    
+        
+        
 
 
     
 
 
-
 def press(btn):
     if btn == "Download":
-        app.clearListBox('lb2')
         optb = app.getOptionBox('Format')
-        if optb == "best" or optb == None:
-            
-            ydl_opts={'format:':optb,'outtml':'%(title)s','nonplaylist':True,'logger':MyLogger()}
+        if optb == "best" or optb == None: 
+            ydl_opts={'format:':optb,'outtml':'%(title)s', 'nonplaylist':True,'logger':MyLogger(),'progress_hooks':[my_hook],'outtmpl':locat+ '%(title)s'}
         else:
-            print "else"
             ydl_opts={'format':optb,'outtml':'%(title)s','noplaylist':True,'logger':MyLogger(), 'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -37,35 +56,20 @@ def press(btn):
         durl = app.getEntry('e1')
         
         def getinfo():
+            global locat
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                mt1 = ydl.extract_info(durl)
-                mt1t = ('Title:' + str(mt1['title'].encode('ascii','replace')) )
-                mt2u = ('Uploader:' + str(mt1['uploader'].encode('ascii','replace')) )
-                mt3v = ('Views:' + str(mt1['view_count']) )
-                mt4d = ('Duration:' + str(mt1['duration']) )
-                mt5ud = ('Upload Date:' + str(mt1['upload_date']) )
-                mt6f = ('Format:' + str(mt1['format']))
-                app.queueFunction(app.addListItems, 'lb2', [ mt1t, mt2u, mt3v,mt4d,mt5ud,mt6f] )
-        if optb == None:
-            optb = 'Video'
-        app.thread(getinfo)
-        app.addListItem('l5','Download Started as' + ' ' + optb + '...')
-
-        def startDownload():
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                optb = app.getOptionBox('Format')
-                if optb == 'best':
-                    optb = 'Video'
-                elif optb == None:
-                        optb = 'Video' 
-                else:
-                    optb = 'Audio'
+                mt1 = ydl.extract_info(durl,download=False)
+                mt1t = (str(mt1['title'].encode('ascii','replace')) )
+                mt2u = (str(mt1['uploader'].encode('ascii','replace')) )
+                mt3v = (str(mt1['view_count']) )
+                mt4d = ((str(mt1['duration'] / 60) + ':'+ str(mt1['duration'] % 60) ) )
+                mt5ud = (str(mt1['upload_date']) )
+                mt6f = (str(mt1['format']))
+                app.addGridRow('lb1',[mt1t,mt2u,mt3v,mt4d,mt5ud,mt6f])
                 ydl.download([durl])
-                mt = ydl.extract_info(durl)
-                mtt= mt['title'].encode('ascii','replace')
-                app.clearEntry('e1')
-                app.addListItem('lb1',(str(mtt) + '-  ////   -'+ str(optb) + ' ' + '-- OK'))
-        app.thread(startDownload)
+                print locat
+
+        app.thread(getinfo) 
 
 
 app.addEntry('e1',0,0)
@@ -76,18 +80,20 @@ app.setButtonFg('Download','green')
 app.addLabel('l2','Downloaded Items',1,0)
 app.setLabelFg('l2','green')
 app.setEntrySticky('e1','news')
-app.addListBox('lb1','',2,0)
-app.addListBox('lb2','',2,1)
-app.addLabel('l3','Datas',1,1)
-app.setLabelFg('l3','green')
-app.addLabelOptionBox('Format',['-Video-','best','-Audio-','bestaudio'],3,0)
+app.addGrid('lb1',[['Title','Uploader','Views','Duration','Upload Date','Format']],2,0,2,2)
+app.setGridSticky('lb1','news')
+app.addLabelOptionBox('Format',['-Video-','best','-Audio-','bestaudio'],3,1)
 app.addLabel('l4','Logger',4,0)
 app.addLabel('l5b','',4,1)
 app.setLabelBg('l5b','green')
 app.setLabelBg('l4','green')
 app.addListBox('l5','',5,0,2,2)
 app.setListBoxSticky('l5','news')
-app.thread(MyLogger)
+app.addMeter('progress',3,0)
+app.setMeterFill('progress','green')
+app.addMenuList('File',['Select Download Folder'],tkfl)
+
+
 app.go()
 
 
